76683495a375a40902da3e4597e717680c34b575
polarfill <polarfil213@gmail.com>
2021-07-08 00:42:37 +0000

    foda1

--- js/mobile.js
+++ js/mobile.js
@@ -1,4 +1,4 @@
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+if ((/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) || (document.referrer != "")) {
     window.location.replace("mobile/indexM.html")
   }else{
     console.log("Script1 funcionado")
